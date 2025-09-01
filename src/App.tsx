e0d801e3053456a11970dacc5d4c8362be577273
dannyminanir <minanidanny28@gmail.com>
2025-09-01 11:14:02 +0200

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 
 
 
-const App: React.FC = () => {
-
-
-  return (
+function App(){  return (
     <div>
       <h1 className="text-blue-900 font-bold text-2xl mt-15 text-center">Mission Track</h1>
     </div>
   );
-};
+}
 
 export default App;
