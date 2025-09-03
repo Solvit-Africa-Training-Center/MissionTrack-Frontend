ce8d0da4fec483f6f63accecfca62b27433c0193
dannyminanir <minanidanny28@gmail.com>
2025-09-03 12:41:19 +0200

    change

---
+++ src/pages/EmployeeDashboard.tsx
@@ -0,0 +1,24 @@
+// <<<<<<< HEAD
+// =======
+
+
+// import Dashboard from "../Components/Dashboard";
+// import NavBar from "../Components/NavBar";
+
+
+
+
+// const EmployeeDashboard: React.FC = ()=>{
+//     return(
+
+//         <div>
+          
+//             <NavBar/>
+//             <Dashboard/>
+            
+//         </div>
+//     );
+// }
+
+// export default EmployeeDashboard;
+// >>>>>>> 73ed0ae1c4427fac3c4824eafedfb217d3eeedf1
