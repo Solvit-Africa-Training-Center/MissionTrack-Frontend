370d91eb17d8cfa1af148269edc11ff72098d98b
dannyminanir <minanidanny28@gmail.com>
2025-09-03 11:54:08 +0200

    e

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-import { Route, Routes, useLocation } from "react-router-dom";
-import Dashboard from "./Components/Dashboard";
-import NavBar from "./Components/NavBar";
-import LoginForm from "./pages/LoginFom";
+import AppRoute from "./Router/AppRoute";
+
 
 function App() {
-  const location = useLocation();
 
-  // Hide navbar on login page
-  const hideNavbar = location.pathname === "/login";
 
   return (
     <div>
-      {!hideNavbar && <NavBar />} {/* only show navbar when not on login page */}
-
-      <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <AppRoute/>
     </div>
   );
 }
