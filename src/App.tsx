2de299bf0bc1692e5421c57900d6cd3cc759c1a0
TCorneille <corneilletwagirimana@gmail.com>
2025-09-03 23:42:20 -0700

    origin dev

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,7 @@
 import AppRoute from "./Router/AppRoute";
 
 
-// const App: React.FC = () => {
-//   const { user } = useAuth();
 
-//   return user ? (
-//     <EmployeeDashboard />  // ← Shows when logged in
-//   ) : (
-//     <LoginForm />          // ← Shows when not logged in
-//   );
-// };
-
-// export default App
-
-
-
-// =======
 function App() {
 
   return (
